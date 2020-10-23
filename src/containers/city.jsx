c50477c9377a9f45a5b96683effc8ed2ff7ffea5
Jocelyn Duquesnoy <jocelyn.duquesnoy@yahoo.fr>
2020-10-23 15:59:48 +0200

    red clicked behaviour

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 class City extends Component { 
  constructor(props) {
  super(props);
+  this.state ={cliked:false}
  }
+ handleClick = () => {
+  this.setState({ clicked: !this.state.clicked });
+  } 
+ 
+
  render() {
   return (
   <li
-  className="list-group-item"
->{this.props.city.name}</li>
+  className={`list-group-item ${this.state.clicked ? "clicked" : ""}`} onClick={this.handleClick}>{this.props.city.name}</li>
 );
  }
 }
